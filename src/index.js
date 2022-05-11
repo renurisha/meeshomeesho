8e41d87b3c8433b5d45d965f4cfd18dd455740d4
Renu kumari <renuk1485@gmail.com>
2022-05-11 23:45:42 +0530

    m

--- src/index.js
+++ src/index.js
@@ -5,12 +5,38 @@ const port = process.env.PORT || 8080;
 const Product = require("./models/productModel");
 
 app.use(express.json());
-
-app.get("", (req, res) => {
-  res.send("hello");
+app.get("/", (req, res) => {
+  res.status(200).json("page not found");
 });
-app.get("/user", (req, res) => {
-  res.send("products.......");
+
+app.post("/product", async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    const createdproduct = await product.save();
+    console.log(createdproduct);
+    res.status(201).send(createdproduct);
+  } catch (e) {
+    res.status(401).send(e);
+  }
+});
+app.get("/product", async (req, res) => {
+  try {
+    const product = await Product.find();
+
+    res.status(200).json(product);
+  } catch (e) {
+    res.status(401).json(e);
+  }
+});
+app.get("/product/:name", async (req, res) => {
+  try {
+    const regex = new RegExp(req.params.name);
+    const product = await Product.find({ name: regex });
+
+    res.status(200).json(product);
+  } catch (e) {
+    res.status(401).json(e);
+  }
 });
 
 app.listen(port, () => {
