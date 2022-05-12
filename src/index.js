ab3887aff59a63e355df91d15181aa9be29905eb
Renu kumari <renuk1485@gmail.com>
2022-05-12 06:00:04 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -9,26 +9,14 @@ app.get("/", (req, res) => {
   res.send("not found......");
 });
 
-app.get("/product", async (req, res) => {
-  try {
-    const product = await Product.find();
-    console.log(product);
-
-    res.send(product);
-  } catch (e) {
-    res.send(e);
-  }
-});
-
-app.get("/product/:name", async (req, res) => {
-  try {
-    const regex = new RegExp(req.params.name);
-    const product = await Product.find({ product_type: regex });
-
-    res.send(product);
-  } catch (e) {
-    res.send(e);
-  }
+app.get("/product", (req, res) => {
+  Product.find()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 });
 
 app.listen(port, () => {
